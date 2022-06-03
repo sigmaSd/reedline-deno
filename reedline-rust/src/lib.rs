46911da70cc4edb5504e1fa494c676faf5c99d9b
Nbiba Bedis <bedisnbiba@gmail.com>
2022-06-04 00:40:42 +0100

    Make reedline safe to use concurently

--- reedline-rust/src/lib.rs
+++ reedline-rust/src/lib.rs
@@ -1,16 +1,25 @@
-use std::ffi::CString;
-
 use reedline::{DefaultPrompt, Reedline, Signal};
+use std::{
+    ffi::CString,
+    mem::ManuallyDrop,
+    sync::{Arc, Mutex},
+};
 
 #[no_mangle]
-pub fn new() -> *mut Reedline {
-    Box::into_raw(Box::new(Reedline::create()))
+pub extern "C" fn new() -> *const Mutex<Reedline> {
+    Arc::into_raw(Arc::new(Mutex::new(Reedline::create())))
 }
+
 #[no_mangle]
-pub fn read_line(editor: *mut Reedline) -> *mut i8 {
-    let sig = unsafe { &mut *editor }
+pub extern "C" fn read_line(editor: *const Mutex<Reedline>) -> *mut i8 {
+    let editor = ManuallyDrop::new(unsafe { Arc::from_raw(editor) });
+
+    let sig = editor
+        .lock()
+        .unwrap()
         .read_line(&DefaultPrompt::default())
         .unwrap();
+
     if let Signal::Success(res) = sig {
         CString::new(res.as_str()).unwrap().into_raw()
     } else {
